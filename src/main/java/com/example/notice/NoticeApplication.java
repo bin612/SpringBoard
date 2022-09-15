bc0ba00b22dde179973aee33f022648c6009829a
kimsangbin <tkdqls612@naver.com>
2022-09-15 21:59:15 +0900

    add : @EnableJpaAuditing 추가

--- src/main/java/com/example/notice/NoticeApplication.java
+++ src/main/java/com/example/notice/NoticeApplication.java
@@ -2,12 +2,13 @@ package com.example.notice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Auditing을 사용하면 @EnableAuditing 스프링 부트 설정 클래스에 적용해야함
 @SpringBootApplication
 public class NoticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoticeApplication.class, args);
 	}
-
 }
